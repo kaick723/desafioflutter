cce5cd59b6e2690c8fdbce92e28cdb3f3e8841d2
Kaick Vasconcelos <86269939+kaick723@users.noreply.github.com>
2023-11-24 23:49:45 -0300

    Add files via upload

--- desafio/index.js
+++ desafio/index.js
@@ -15,3 +15,7 @@ function sumMultiplesOf3and5(number) {
   const result = sumMultiplesOf3and5(10);
   console.log(result); // Should print 23, as 3 + 5 + 6 + 9 are the numbers less than 10 and divisible by 3 or 5.
   
+  const result1 = sumMultiplesOf3and5(11);
+  console.log(result1);// Should print 33, as 3 + 5 + 6 + 9 + 10 are the numbers less than 11 and divisible by 3 or 5.
+
+
